2a043b902446b58793eb74f3d48dd50dd47808b5
qcavaille <q.cavaille@gmail.com>
2017-01-22 18:09:58 +0100

    a tester

--- model/Model.py
+++ model/Model.py
@@ -1,5 +1,19 @@
+from subprocess import call
 
 class Model():
-
-	def __init__(self):
-		self.command = "deBGA"
+    def __init__(self,cmd): #cmd = array of string
+        self.command = "deBGA" 
+        if self.launched:
+            return "ERROR"
+        else:
+           for i in cmd:
+                self.command=self.command+ " " +i
+                call(self.command)
+        
+        self.command=[] #clear attribute "command" for launching other run
+    
+    def launched(self): #test if command already launched, return true if running
+        launch=call("ps aux |grep "+self.command)#launch contains return code
+        if launch !="0":
+            return False
+        return True
