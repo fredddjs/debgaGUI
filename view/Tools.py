d7c4c20d6218c8b6db6af6d255fafa5ee9bed8a1
qcavaille <q.cavaille@gmail.com>
2017-01-23 14:00:26 +0100

    modifié :         localisation/english.txt 	modifié :         view/Tools.py

--- view/Tools.py
+++ view/Tools.py
@@ -1,6 +1,7 @@
 import gtk
 from Index import Index
 from Alignment import Alignment
+from Viewer import Viewer
 from localisation import config
 
 class Tools(gtk.Notebook):
@@ -16,11 +17,19 @@ class Tools(gtk.Notebook):
 		aAlign = gtk.Alignment(0.5,0.25, 0, 0)
 		aAlign.add( Alignment() )
 
+        #Viewer notebook
+		vboxViewer = gtk.VBox(False, 5)
+		aViewer = gtk.Alignment(0.5,0.25, 0, 0)
+		aViewer.add( Viewer() )
+		
 		vboxIndex.pack_start( aIndex )
 		vboxAlign.pack_start( aAlign )
+		vboxViewer.pack_start( aViewer )
 		
 		self.set_tab_pos(gtk.POS_TOP)        		
 		self.append_page(vboxIndex)
 		self.set_tab_label_text(vboxIndex, config.LOC["tools_index"])
 		self.append_page(vboxAlign)
 		self.set_tab_label_text(vboxAlign, config.LOC["tools_align"])
+		self.append_page(vboxViewer)
+		self.set_tab_label_text(vboxViewer, config.LOC["tools_viewer"])
